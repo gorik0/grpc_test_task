bc581db455d5c908b67706a5e2d3b1a319d9c2f7
gorik0 <delavarus@gmail.com>
2024-07-25 10:15:07 +0300

    grpc_done

---
+++ pkg/user/grpc/grpc.go
@@ -0,0 +1,84 @@
+package grpc
+
+import (
+	"context"
+	g "grpc/pkg/user/grpc/userservice"
+	"grpc/pkg/user/model"
+	"grpc/pkg/user/service"
+	"log"
+)
+
+type GRPC struct {
+	g.UnimplementedUserServiceServer
+	Services service.Services
+}
+
+func (G *GRPC) CreateUser(ctx context.Context, u *g.User) (*g.Reply, error) {
+	id, err := G.Services.CreateUser(ctx, u.Email)
+	if err != nil {
+		return &g.Reply{
+			Id:     "",
+			Status: err.Error(),
+		}, err
+
+	} else {
+		log.Default().Println("CreateUser success")
+		err = G.Services.LogNewUser(ctx)
+		if err != nil {
+			log.Default().Println("error while logging new user", err.Error())
+		}
+		return &g.Reply{
+			Id:     id,
+			Status: "OK",
+		}, nil
+	}
+}
+func (G *GRPC) DropUser(ctx context.Context, u *g.User) (*g.Reply, error) {
+	err := G.Services.DeleteUser(ctx, u.Id)
+	if err != nil {
+		return &g.Reply{
+			Status: err.Error(),
+		}, err
+
+	} else {
+		return &g.Reply{
+			Status: "DELETED",
+		}, nil
+	}
+
+}
+func (G *GRPC) GetUsers(ctx context.Context, u *g.SelectParams) (*g.UserList, error) {
+	//	TRYin get user from cached then ( else from db and set to cache)
+	var users []*model.User
+	users, err := G.Services.GetUsersCached(ctx, int(u.Limit), int(u.Offset))
+	switch {
+	case err != nil:
+		return nil, err
+	case users == nil:
+		{
+
+			users, err = G.Services.GetUsers(ctx, int(u.Limit), int(u.Offset))
+			if err != nil {
+				return nil, err
+			}
+			err = G.Services.SetUsersCached(ctx, int(u.Limit), int(u.Offset), users)
+			if err != nil {
+				return nil, err
+			}
+
+		}
+
+	}
+	usersG := []*g.User{}
+
+	for i, user := range users {
+		usersG[i] = &g.User{
+			Id:    user.ID,
+			Email: user.Email,
+		}
+
+	}
+	return &g.UserList{
+		User: usersG,
+	}, err
+}
--- pkg/user/service/service.go
+++ pkg/user/service/service.go
@@ -1,1 +1,86 @@
 package service
+
+import (
+	"context"
+	"grpc/pkg/user/cache"
+	"grpc/pkg/user/logger"
+	"grpc/pkg/user/model"
+	"grpc/pkg/user/storage"
+	"strconv"
+)
+
+type Services interface {
+	CreateUser(ctx context.Context, email string) (id string, err error)
+	DeleteUser(ctx context.Context, idUser string) error
+	GetUsers(ctx context.Context, limit, offset int) ([]*model.User, error)
+	GetUsersCached(ctx context.Context, limit, offset int) ([]*model.User, error)
+	SetUsersCached(ctx context.Context, limit, offset int, users []*model.User) error
+	LogNewUser(ctx context.Context) error
+}
+type Service struct {
+	storage storage.CRUDL
+	cacher  cache.Cacher
+	logger  logger.Logger
+}
+
+func (s Service) CreateUser(ctx context.Context, email string) (id string, err error) {
+	user := model.UserDB{
+		Email: email,
+	}
+
+	idInt, err := s.storage.CreateUser(ctx, &user)
+	if err != nil {
+		return "", err
+
+	}
+	id = strconv.Itoa(idInt)
+	return id, nil
+
+}
+
+func (s Service) DeleteUser(ctx context.Context, idUser string) error {
+	id, _ := strconv.Atoi(idUser)
+	err := s.storage.DeleteUser(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s Service) GetUsers(ctx context.Context, limit, offset int) ([]*model.User, error) {
+	all, err := s.storage.GetAll(ctx, limit, offset)
+	if err != nil {
+		return nil, err
+
+	}
+	users := make([]*model.User, len(all))
+	for i, user := range all {
+		users[i] = &model.User{
+			ID:    user.ID,
+			Email: user.Email,
+		}
+
+	}
+	return users, err
+}
+
+func (s Service) GetUsersCached(ctx context.Context, limit, offset int) ([]*model.User, error) {
+	return s.cacher.GetList(ctx, limit, offset)
+
+}
+
+func (s Service) SetUsersCached(ctx context.Context, limit, offset int, users []*model.User) error {
+	return s.cacher.SetList(ctx, users, limit, offset)
+}
+
+func (s Service) LogNewUser(ctx context.Context) error {
+	return s.logger.LoNewUser(ctx)
+}
+
+func NewServices(storage storage.CRUDL, cacher cache.Cacher, logger logger.Logger) Services {
+	return &Service{
+		storage: storage,
+		cacher:  cacher,
+		logger:  logger,
+	}
+}
